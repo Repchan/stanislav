4ae0b432f6b8a9e187761231c2eb978ac0905127
Repchan <96149619+Repchan@users.noreply.github.com>
2024-01-03 22:47:25 +0200

    Some change

--- src/components/Course.js
+++ src/components/Course.js
@@ -3,8 +3,10 @@ import React from 'react';
 class Course extends React.Component{
     render(){
         return(
-            <div className={'Course'}>
-                <p>Напрямок</p>
+            <div className={'course'}>
+                <div className={'container'}>
+                    <h1>Напрямок</h1>
+                </div>
             </div>
         )
     }
